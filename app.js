970e962460b35c9e8b5140f6ccdae07b2b9c4b7b
Marcos Sosa <marcossosa.js@gmail.com>
2023-04-20 14:17:47 -0400

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const router = express.Router();
+
+const puerto = process.env.puerto;
+
+
+app.use(express.json());
+app.use(express.static(__dirname + "/src/pages"));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/src/pages/index.html');
+});
+
+app.listen(puerto, () => {
+    console.log(`server escuchando en puerto ${puerto}`);
+});
