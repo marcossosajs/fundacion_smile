41ab53f9e7f275f5ab91b817d5bf959b688f69c9
Marcos Sosa <marcossosa.js@gmail.com>
2023-04-26 00:38:36 -0400

    update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const puerto = 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname + "/src/"));
-
+ 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/src/pages/index.html');
 });
